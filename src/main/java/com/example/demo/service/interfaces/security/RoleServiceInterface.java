a256b031598b87d889d969288a94b64907c4adbe
beatriz-perez <beatriz.pfp@gmail.com>
2022-05-17 17:40:30 +0200

    Updates RoleService and interface

--- src/main/java/com/example/demo/service/interfaces/security/RoleServiceInterface.java
+++ src/main/java/com/example/demo/service/interfaces/security/RoleServiceInterface.java
@@ -1,4 +1,11 @@
 package com.example.demo.service.interfaces.security;
 
+import com.example.demo.model.security.Role;
+
 public interface RoleServiceInterface {
+
+    Role saveRole(Role role);
+
+    void addRoleToUser(Long userId, String roleName);
+
 }
