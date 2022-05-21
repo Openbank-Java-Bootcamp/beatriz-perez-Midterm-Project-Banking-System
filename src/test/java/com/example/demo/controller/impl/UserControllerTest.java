200f20507fed77fe6ec605439eb10ad65cc8b012
beatriz-perez <beatriz.pfp@gmail.com>
2022-05-21 18:52:06 +0200

    Adds tests for uuser controller

--- src/test/java/com/example/demo/controller/impl/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/impl/UserControllerTest.java
@@ -1,64 +1,183 @@
 package com.example.demo.controller.impl;
 
+import com.example.demo.model.secondary.Address;
+import com.example.demo.model.secondary.Name;
+import com.example.demo.model.users.AccountHolder;
+import com.example.demo.model.users.ThirdParty;
+import com.example.demo.model.users.User;
+import com.example.demo.repository.users.AccountHolderRepository;
+import com.example.demo.repository.users.ThirdPartyRepository;
+import com.example.demo.repository.users.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
+@SpringBootTest
 class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private AccountHolderRepository accountHolderRepo;
+    @Autowired
+    private ThirdPartyRepository thirdPartyRepo;
+
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        List<User> users = userRepo.saveAll(
+                List.of(
+                        new User(new Name("Rita", "Hayworth"), "ritahayworth", "gilda" ),
+                        new User(new Name("James", "Dean"), "jamesdean", "eastofeden" )
+                )
+        );
+        List<ThirdParty> thirdParties = thirdPartyRepo.saveAll(
+                List.of(
+                        new ThirdParty("clarkgable", "butler"),
+                        new ThirdParty("vivienleigh", "ohara")
+                )
+        );
+        List<AccountHolder> accountHolders = accountHolderRepo.saveAll(
+                List.of(
+                        new AccountHolder(
+                                new Name("Scarlett", "Ohara"),
+                                "scarlettohara",
+                                "wind",
+                                "1913-11-05",
+                                new Address("Tara", "Georgia", "USA", "54321")
+                        )
+                )
+        );
     }
 
     @AfterEach
     void tearDown() {
+        userRepo.deleteAll();
+        thirdPartyRepo.deleteAll();
     }
 
     @Test
-    void getAllUsers() {
+    void getAllUsers() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk()).andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("Hayworth"));
+        assertTrue(result.getResponse().getContentAsString().contains("Dean"));
+        assertTrue(result.getResponse().getContentAsString().contains("Ohara"));
     }
 
     @Test
-    void getUserById() {
+    void getUserById() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk()).andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("Hayworth"));
     }
 
     @Test
-    void getAllThirdParties() {
+    void getAllThirdParties() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/users/third-party"))
+                .andExpect(status().isOk()).andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("gable"));
+        assertTrue(result.getResponse().getContentAsString().contains("leigh"));
     }
 
     @Test
-    void createUser() {
+    void createUser() throws Exception {
+        String body = objectMapper.writeValueAsString( new User(new Name("Marlon", "Brando"), "marlonbrando", "godfather") );
+        mockMvc.perform(post("/api/users/admin-user").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
     }
 
     @Test
-    void createAccountHolder() {
+    void createAccountHolder() throws Exception {
+        String body = objectMapper.writeValueAsString(
+                new AccountHolder(
+                        new Name("Marilyn", "Monroe"),
+                        "marilynmonroe",
+                        "niagara",
+                        "1926-06-01",
+                        new Address("BrentWood", "LA", "USA", "01234")
+                )
+        );
+        mockMvc.perform(post("/api/users/account-holder").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
     }
 
     @Test
-    void createThirdParty() {
+    void createThirdParty() throws Exception {
+        String body = objectMapper.writeValueAsString(
+                new ThirdParty(
+                        "marilynmonroe",
+                        "niagara"
+                )
+        );
+        mockMvc.perform(post("/api/users/third-party").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
     }
 
     @Test
-    void updateUserById() {
+    void updateUserById() throws Exception {
+        String body = objectMapper.writeValueAsString( new User(new Name("Ms Rita", "Hayworth"), "ritahayworth", "gilda" ) );
+        mockMvc.perform(put("/api/users/admin-user/1").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
+        assertEquals(userRepo.findById(1L).get().getName().getFirstName(), "Ms Rita");
     }
 
     @Test
-    void updateAccountHolderById() {
+    void updateAccountHolderById() throws Exception {
+        String body = objectMapper.writeValueAsString(
+                new AccountHolder(
+                        new Name("Ms Scarlett", "Ohara"),
+                        "scarlettohara",
+                        "wind",
+                        "1913-11-05",
+                        new Address("Tara", "Georgia", "USA", "54321")
+                )
+        );
+        mockMvc.perform(put("/api/users/account-holder/1").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
+        assertEquals(accountHolderRepo.findById(1L).get().getName().getFirstName(), "Ms Scarlett");
     }
 
     @Test
-    void updateThirdPartyById() {
+    void updateThirdPartyById() throws Exception {
+        String body = objectMapper.writeValueAsString( new ThirdParty( "rhettbutler", "butler" ) );
+        mockMvc.perform(put("/api/users/third-party/1").content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
+        assertEquals(thirdPartyRepo.findById(1L).get().getUsername(), "rhettbutler");
     }
 
     @Test
-    void deleteUserById() {
+    void deleteUserById() throws Exception {
+        mockMvc.perform(delete("/api/users/1"))
+                .andExpect(status().isNoContent());
     }
 
     @Test
-    void deleteThirdPartyById() {
+    void deleteThirdPartyById() throws Exception {
+        mockMvc.perform(delete("/api/users/third-party/1"))
+                .andExpect(status().isNoContent());
     }
 }
